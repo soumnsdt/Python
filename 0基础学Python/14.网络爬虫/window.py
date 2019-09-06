867d0ee328b093216daac6de1bf88efa1e51cd23
soumnsdt <30397118+soumnsdt@users.noreply.github.com>
2019-09-07 07:16:33 +0800

    爬虫界面更新
    爬虫界面更新

--- 0基础学Python/14.网络爬虫/window.py
+++ 0基础学Python/14.网络爬虫/window.py
@@ -8,7 +8,8 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys      # 导入系统模块
+import sys
+from PyQt5.QtGui import  *
 
 
 class Ui_MainWindow(object):
@@ -19,12 +20,24 @@ class Ui_MainWindow(object):
         MainWindow.setMaximumSize(QtCore.QSize(960, 700))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        # 1、通过label控件显示顶部位图
         self.label_title_img = QtWidgets.QLabel(self.centralwidget)
         self.label_title_img.setGeometry(QtCore.QRect(0, 0, 960, 141))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_title_img.setFont(font)
         self.label_title_img.setObjectName("label_title_img")
+        title_img = QPixmap('img/bg1.png')
+        self.label_title_img.setPixmap(title_img)
+        # 2、查询部分的widget
         self.widget_query = QtWidgets.QWidget(self.centralwidget)
         self.widget_query.setGeometry(QtCore.QRect(0, 141, 960, 80))
         self.widget_query.setObjectName("widget_query")
+        self.widget_query.setAutoFillBackground(True)    # 开启自动填充背景
+        palette = QPalette()                             # 调色板类
+        palette.setBrush(QPalette.Background, QtGui.QBrush(QtGui.QPixmap('img/bg2.png')))  # 设置背景图片
+        self.widget_query.setPalette(palette)            # 为控件设置对应的调色板即可
+
         self.label = QtWidgets.QLabel(self.widget_query)
         self.label.setGeometry(QtCore.QRect(30, 30, 54, 12))
         self.label.setObjectName("label")
@@ -46,30 +59,33 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.widget_query)
         self.pushButton.setGeometry(QtCore.QRect(600, 25, 75, 25))
         self.pushButton.setObjectName("pushButton")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(0, 221, 960, 100))
-        self.widget.setObjectName("widget")
-        self.label_4 = QtWidgets.QLabel(self.widget)
-        self.label_4.setGeometry(QtCore.QRect(30, 22, 54, 12))
-        self.label_4.setObjectName("label_4")
-        self.checkBox = QtWidgets.QCheckBox(self.widget)
-        self.checkBox.setGeometry(QtCore.QRect(100, 22, 71, 16))
-        self.checkBox.setObjectName("checkBox")
-        self.checkBox_2 = QtWidgets.QCheckBox(self.widget)
-        self.checkBox_2.setGeometry(QtCore.QRect(250, 22, 71, 16))
-        self.checkBox_2.setObjectName("checkBox_2")
-        self.checkBox_3 = QtWidgets.QCheckBox(self.widget)
-        self.checkBox_3.setGeometry(QtCore.QRect(400, 22, 71, 16))
-        self.checkBox_3.setObjectName("checkBox_3")
-        self.checkBox_4 = QtWidgets.QCheckBox(self.widget)
-        self.checkBox_4.setGeometry(QtCore.QRect(550, 22, 71, 16))
-        self.checkBox_4.setObjectName("checkBox_4")
-        self.checkBox_5 = QtWidgets.QCheckBox(self.widget)
-        self.checkBox_5.setGeometry(QtCore.QRect(700, 22, 71, 16))
-        self.checkBox_5.setObjectName("checkBox_5")
-        self.listView = QtWidgets.QListView(self.centralwidget)
-        self.listView.setGeometry(QtCore.QRect(5, 325, 950, 370))
-        self.listView.setObjectName("listView")
+        self.widget_checkbox = QtWidgets.QWidget(self.centralwidget)
+        self.widget_checkbox.setGeometry(QtCore.QRect(0, 221, 960, 50))
+        self.widget_checkbox.setObjectName("widget_checkbox")
+        self.label_type = QtWidgets.QLabel(self.widget_checkbox)
+        self.label_type.setGeometry(QtCore.QRect(30, 20, 54, 12))
+        self.label_type.setObjectName("label_type")
+        self.checkBox_G = QtWidgets.QCheckBox(self.widget_checkbox)
+        self.checkBox_G.setGeometry(QtCore.QRect(100, 20, 71, 16))
+        self.checkBox_G.setObjectName("checkBox_G")
+        self.checkBox_D = QtWidgets.QCheckBox(self.widget_checkbox)
+        self.checkBox_D.setGeometry(QtCore.QRect(250, 20, 71, 16))
+        self.checkBox_D.setObjectName("checkBox_D")
+        self.checkBox_Z = QtWidgets.QCheckBox(self.widget_checkbox)
+        self.checkBox_Z.setGeometry(QtCore.QRect(400, 20, 71, 16))
+        self.checkBox_Z.setObjectName("checkBox_Z")
+        self.checkBox_T = QtWidgets.QCheckBox(self.widget_checkbox)
+        self.checkBox_T.setGeometry(QtCore.QRect(550, 20, 71, 16))
+        self.checkBox_T.setObjectName("checkBox_T")
+        self.checkBox_K = QtWidgets.QCheckBox(self.widget_checkbox)
+        self.checkBox_K.setGeometry(QtCore.QRect(700, 20, 71, 16))
+        self.checkBox_K.setObjectName("checkBox_K")
+        self.label_train_img = QtWidgets.QLabel(self.centralwidget)
+        self.label_train_img.setGeometry(QtCore.QRect(0, 271, 960, 62))
+        self.label_train_img.setObjectName("label_train_img")
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(0, 333, 960, 365))
+        self.tableView.setObjectName("tableView")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -78,27 +94,25 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "快手爬票"))
-        self.label_title_img.setText(_translate("MainWindow", "TextLabel"))
         self.label.setText(_translate("MainWindow", "出发地："))
         self.label_2.setText(_translate("MainWindow", "目的地："))
         self.label_3.setText(_translate("MainWindow", "出发日："))
         self.pushButton.setText(_translate("MainWindow", "查询"))
-        self.label_4.setText(_translate("MainWindow", "车次类型："))
-        self.checkBox.setText(_translate("MainWindow", "GC-高铁"))
-        self.checkBox_2.setText(_translate("MainWindow", "D-动车"))
-        self.checkBox_3.setText(_translate("MainWindow", "Z-直达"))
-        self.checkBox_4.setText(_translate("MainWindow", "T-特快"))
-        self.checkBox_5.setText(_translate("MainWindow", "K-快速"))
+        self.label_type.setText(_translate("MainWindow", "车次类型："))
+        self.checkBox_G.setText(_translate("MainWindow", "GC-高铁"))
+        self.checkBox_D.setText(_translate("MainWindow", "D-动车"))
+        self.checkBox_Z.setText(_translate("MainWindow", "Z-直达"))
+        self.checkBox_T.setText(_translate("MainWindow", "T-特快"))
+        self.checkBox_K.setText(_translate("MainWindow", "K-快速"))
 
 
 def show_MainWindow():
-    app = QtWidgets.QApplication(sys.argv)    # 实例化QApplication类，作为GUI主程序入口
-    MainWindow = QtWidgets.QMainWindow()      # 创建QMainWindow类
-    ui = Ui_MainWindow()                      # 实例化UI类
-    ui.setupUi(MainWindow)                    # 设置窗体UI
-    MainWindow.show()                         # 显示窗体
-    sys.exit(app.exec_())                     # 当窗体创建完成之后，需要结束主循环过程
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     show_MainWindow()
